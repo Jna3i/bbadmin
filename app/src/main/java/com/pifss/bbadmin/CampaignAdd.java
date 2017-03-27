79fde6b007dbfb8cd583728c970235edd1338bcd
ios <ios>
2017-03-27 22:00:20 +0300

    campaign

---
+++ app/src/main/java/com/pifss/bbadmin/CampaignAdd.java
@@ -0,0 +1,13 @@
+package com.pifss.bbadmin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CampaignAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_campaign_add);
+    }
+}
