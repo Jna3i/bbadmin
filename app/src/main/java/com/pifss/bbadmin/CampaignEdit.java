6aa538bad3ba09c502c1894c800f92f6d4af5d42
ios <ios>
2017-03-28 11:39:54 +0300

    toolbar and campaign list

--- app/src/main/java/com/pifss/bbadmin/CampaignEdit.java
+++ app/src/main/java/com/pifss/bbadmin/CampaignEdit.java
@@ -1,7 +1,10 @@
 package com.pifss.bbadmin;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class CampaignEdit extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class CampaignEdit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campaign_edit);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.campEdit_toolbarID);
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setTitle("Edit Campaign");
+        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
