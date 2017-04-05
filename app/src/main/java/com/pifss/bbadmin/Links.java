96e1c50fc4fa70999b849b28f583915908911c21
Jna3i <ahmedalqinai@gmai.com>
2017-04-05 08:46:05 +0300

    added Links java class

---
+++ app/src/main/java/com/pifss/bbadmin/Links.java
@@ -0,0 +1,27 @@
+package com.pifss.bbadmin;
+
+/**
+ * Created by ahmed on 05/04/17.
+ */
+
+public class Links {
+
+    //bbadmin
+    public static String BASE = "http://34.196.107.188:8081/MhealthWeb/webresources";
+    public static String BBADMIN = BASE + "/bbadmin";
+    public static String BBADMIN_LOGIN = BBADMIN + "/login";
+
+    //Campaign (callfordonation)
+    public static String CAMPAIGN = BASE + "/callfordonation";
+
+    //Schedule (Peroid Adapter)
+    public static String SCHEDULE = BASE + "/schedule";//+"/branch/"+id+"/day/"+date
+
+    //bbnotification
+    public static String BBNOTIFICATION = BASE + "/bbnotification";
+
+    //onsitedonor
+    public static String ONSITEDONOR = BASE + "/onsitedonor";
+    //OLD
+    public static String ONSITEDONOR_OLD = "http://34.196.107.188:8080/mHealthWS/ws/onsitedonor";
+}
